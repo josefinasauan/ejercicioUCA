8e456ffac4b7933d2d2aaf145badfec8975cb49a
josefinasauan <sauanjosefina78@gmail.com>
2025-04-03 19:30:54 -0300

    Add files via upload

---
+++ frutas.py
@@ -0,0 +1,9 @@
+frutas = ["manzana", "pera", "uva", "mandarina"]
+frutas.append("banana")
+frutas.insert(2,"durazno")
+frutas.remove("uva")
+frutas.sort()
+frutas.reverse()
+for fruta in frutas:
+    print(fruta)
+    
